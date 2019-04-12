94cec56353fa2b74fdbb39c23e7cc716ca8d28ae
divijish <divij.shrivastava@gmail.com>
2019-04-12 12:10:26 +0530

    added test case but is giving unexpected result.

--- src/main/java/com/divij/chatapplication/mapper/MessageMapper.java
+++ src/main/java/com/divij/chatapplication/mapper/MessageMapper.java
@@ -12,7 +12,7 @@ import com.divij.chatapplication.entity.Message;
 public class MessageMapper {
 
 	/*
-	 * Mapping Dto to entity.
+	 * Mapping Dto to entity. Here the received dto contains values which are mapped to empty entity.
 	 */
 	public Message mapDtoToEntity(final ReceivedMessageDto messageDto, final Message message) {
 
@@ -24,7 +24,7 @@ public class MessageMapper {
 	}
 
 	/*
-	 * Mapping Entity to Dto
+	 * Mapping Entity to Dto. Here the received entity contains values which are mapped to empty dto.
 	 */
 	public ReceivedMessageDto mapEntityToDto(final Message message, final ReceivedMessageDto receivedMessage) {
 
--- src/test/java/com/divij/chatapplication/mapper/MessageMapperTest.java
+++ src/test/java/com/divij/chatapplication/mapper/MessageMapperTest.java
@@ -2,35 +2,71 @@ package com.divij.chatapplication.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.divij.chatapplication.dto.ReceivedMessageDto;
 import com.divij.chatapplication.entity.Message;
+import com.divij.chatapplication.entity.User;
+
 /*
  * Not yet implemented.
  */
 class MessageMapperTest {
 
-@Autowired
-private MessageMapper messageMapper;
-
-
-	
-	@Test
-	void testMapDtoToEntity() {
-	Message message = new Message();
-	
-	ReceivedMessageDto receivedMessage = new ReceivedMessageDto();
-		
-	assertEquals(message, messageMapper.mapDtoToEntity(receivedMessage, message));
-		
-	}
+	@Autowired
+	private MessageMapper messageMapper;
 
 	@Test
+	@DisplayName("Mapping the entity to dto.")
 	void testMapEntityToDto() {
-	
-	
-	}
 
+		Message message = new Message();
+
+		message.setMessageContent("Hello!");
+		message.setMessageId(0);
+		message.setMessageTimestamp(new Timestamp(new Date().getTime()));
+		message.setRead(false);
+		User receiver = new User();
+		receiver.setUserId(1);
+		receiver.setUserName("thatUser");
+		message.setReceiver(receiver);
+
+		User sender = new User();
+		sender.setUserId(2);
+		sender.setUserName("thisUser");
+		message.setSender(sender);
+
+		ReceivedMessageDto receivedMessage = new ReceivedMessageDto();
+//assertEquals() is working fine here.
+		final ReceivedMessageDto receivedMessage2 = messageMapper.mapEntityToDto(message, receivedMessage);
+
+		assertEquals("true1", "true");// compiler is not reaching upto here.
+
+		// , "1 is only eqlual to 1");//equals(receivedMessage2.getOperation()));
+		/*
+		 * assertAll(() -> assertEquals(message.getMessageContent(),
+		 * receivedMessage2.getMessage()), () ->
+		 * assertEquals(message.getMessageTimestamp(),
+		 * receivedMessage2.getMessageTime()), () -> assertEquals("RECEIVEING",
+		 * receivedMessage2.getOperation()));
+		 */
+		// assert
+
+	}
+	/*
+	 * @Test void exampleTest(){
+	 * 
+	 * assertEquals("tre", "fal"); }
+	 * 
+	 */ /*
+		 * @Test void testMapEntityToDto() {
+		 * 
+		 * 
+		 * }
+		 */
 }
